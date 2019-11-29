d623bbdffa946f0f730b614457935163d85b39cf
eceolas <compagno.e@gmail.com>
2019-11-29 12:57:23 +0000

    cleaning

--- api/api.py
+++ api/api.py
@@ -6,7 +6,6 @@ import joblib
 import numpy as np
 from diamond_util import label_cat_encode
 import pandas as pd
-# https://towardsdatascience.com/deploying-a-machine-learning-model-as-a-rest-api-4a03b865c166
 
 # Model
 model_filename = '../models/model_Gradient_boosted_regression.pkl'
@@ -30,15 +29,15 @@ def predict():
     for cat in cat_columns:
         x=label_cat_encode(x, cat).copy()
         x[cat] = x[cat].astype(int)
-   
+
     y_true = x['price'].values
     y_pred = np.round(model.predict(x.drop('price', axis=1).values), 2)
     output= {'Price predicted': str(y_pred[0]), 'Price true value': str(y_true[0])}
 
     return jsonify(output)
 
-    
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
